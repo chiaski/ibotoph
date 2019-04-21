f7071e41b8d031dcb2c3e9890d5047b00833afd2
Chia <chiamisola@gmail.com>
2019-04-20 22:32:47 -0400

    Add tooltip and jQuery UI

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(function () {
+    $('.tooltip').tooltip({
+        open: function () {
+            // make sure all other tooltips are closed when opening a new one
+            $('.tooltip').not(this).tooltip('close');
+        }
+    }).on('mouseleave', function (e) {
+        var that = this;
+        // close the tooltip later (maybe ...)
+        mouseLeaveTimer = setTimeout(function () {
+            $(that).tooltip('close');
+        }, 100);
+        // prevent tooltip widget to close the tooltip now
+        e.stopImmediatePropagation();
+    });
 
+    $(document).on('mouseenter', '.ui-tooltip', function (e) {
+        // cancel tooltip closing on hover
+        clearTimeout(mouseLeaveTimer);
+    });
+    $(document).on('mouseleave', '.ui-tooltip', function () {
+        // make sure tooltip is closed when the mouse is gone
+        $('.tooltip').tooltip('close');
+    });
+});
